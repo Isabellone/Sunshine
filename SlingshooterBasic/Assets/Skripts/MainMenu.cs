3cfcbc635873dddd22116efdd9890023d6337c75
Isabellone <isi.fenot@yahoo.de>
2015-07-14 18:27:25 +0200

    Sunshine Game
    My sunshine game

---
+++ SlingshooterBasic/Assets/Skripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI () {
+
+		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 10), "Start the Game")) {
+
+			Application.LoadLevel ("SlingshooterScene");
+
+		}
+
+		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 2, Screen.width / 5, Screen.height / 10), "erm...no?")) {
+			
+			Application.LoadLevel ("Start...?");
+			
+		}
+
+	}
+}
